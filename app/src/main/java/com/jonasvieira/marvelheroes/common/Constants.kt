79176d864d6e82b17b7b6f6061a87f990c3f334a
jonvieira <jonasvieira.ti@gmail.com>
2018-12-28 12:15:02 -0200

    create retrofit

--- app/src/main/java/com/jonasvieira/marvelheroes/common/Constants.kt
+++ app/src/main/java/com/jonasvieira/marvelheroes/common/Constants.kt
@@ -2,3 +2,4 @@ package com.jonasvieira.marvelheroes.common
 
 const val API_KEY = "YOUR_API_KEY_HERE"
 const val PRIVATE_KEY = "YOUR_PRIVATE_KEY_HERE"
+const val BASE_URL = "http://gateway.marvel.com/v1/public/"
